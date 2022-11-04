300d0829b2e83fa89402c769c8688ba689a1f063
Matthias Reining <matthias.reining@tech11.com>
2022-11-04 22:10:22 +0100

    FIX watchtower (now :-))

--- src/main/java/de/fhws/business/rooms/boundary/RoomResource.java
+++ src/main/java/de/fhws/business/rooms/boundary/RoomResource.java
@@ -26,7 +26,7 @@ public class RoomResource {
 	@GET
 	@Path("ping")
 	public String ping() {
-		return "I'm here at FHWS 👍💪😊🍻🙈 and hello from Wildfly at " + new Date();
+		return "I'm here at FHWS 👍 and hello from Wildfly at " + new Date();
 	}
 
 	@POST

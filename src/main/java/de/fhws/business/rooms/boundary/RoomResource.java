ed7c97a364dd6d5752e8960530c6fee08b779b27
Matthias Reining <matthias.reining@tech11.com>
2022-11-10 10:28:28 +0100

    update wildfly version

--- src/main/java/de/fhws/business/rooms/boundary/RoomResource.java
+++ src/main/java/de/fhws/business/rooms/boundary/RoomResource.java
@@ -26,7 +26,7 @@ public class RoomResource {
 	@GET
 	@Path("ping")
 	public String ping() {
-		return "I'm here at FHWS 👍 and hello from Wildfly at " + new Date();
+		return "I'm here at FHWS 👍 - new block, new luck " + new Date();
 	}
 
 	@POST
